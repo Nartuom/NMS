f34ec052fcf66ecf737cb00779626e4d32ec9e21
Nartuom <thomas.burton.lawl@gmail.com>
2020-08-10 00:14:53 +0100

    add home page and blog input page. Also add  index, blog, and blogPOST routes

---
+++ public/scripts/nms.js
@@ -0,0 +1,18 @@
+const navToggle = document.getElementById("toggle-button");
+const navNoodles = document.getElementsByClassName("bar");
+const navBar = document.querySelector("nav");
+const links = document.getElementsByClassName("navbar-links");
+
+navToggle.addEventListener("click", () => {
+    console.log("jello");
+    [...navNoodles].forEach(function(noodle){
+        noodle.classList.toggle("onClick");
+    });
+    [...links].forEach(function(link){
+        link.classList.toggle("linksActive");
+    })
+    navNoodles[1].classList.toggle("slip");
+    navBar.classList.toggle("navActive");
+    
+});
+              
